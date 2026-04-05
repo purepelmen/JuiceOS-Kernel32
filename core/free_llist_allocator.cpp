d235aef48dd6b24faae5fd89afc869e063e5400a
purepelmen <dan.fedotov45@gmail.com>
2026-04-05 14:51:51 +0300

    Allocator doesn't maintain free blocks index anymore
    It emplaces the information about free blocks just inside them. This though required increasing the minimum alloc size: previously 9 bytes min (8 byte header + 1 byte of minimal alloc space), now - 12 bytes to be able to fit `llist_free_entry` on free().
    
    This all basically means there's no more restriction of 256 maximum of free blocks total. Now it can probably be *infinite*. Plus as before more non-contigious heap areas can be added if needed to expand the heap size.
    
    This implementation still may be not very performant. In the future some kind of additonal index storage should be added for sorting to speed things up.
    
    Also the allocator still doesn't guarantee any alignment. May be also needed to be fixed.

--- core/free_llist_allocator.cpp
+++ core/free_llist_allocator.cpp
@@ -1,5 +1,6 @@
 #include "free_llist_allocator.h"
 
+#include "math.h"
 #include "stdlib.h"
 #include "console.h"
 
@@ -8,26 +9,10 @@ struct llist_free_entry
     llist_free_entry* prev = nullptr;
     llist_free_entry* next = nullptr;
 
-    void* address;
     size_t size;
-
-    void shrink(size_t amount)
-    {
-        address = address + amount;
-        size -= amount;
-    }
-
-    void growUp(size_t amount)
-    {
-        address = address - amount;
-        size += amount;
-    }
-
-    bool is_empty() const { return address == nullptr; }
 };
 
-const uint32 ALLOC_HEADER_CHECKSUM = 0xDEAD1234;
-const uint32 ALLOC_HEADER_FREED_CHECKSUM = 0xDEADDEAD;
+const uint32 ALLOC_HEADER_CHECKSUM = 0xCAFE1234;
 
 struct alloc_header
 {
@@ -35,31 +20,31 @@ struct alloc_header
     size_t size;
 };
 
-const size_t FREE_LIST_MAXSIZE = 256;
-static llist_free_entry freeList[FREE_LIST_MAXSIZE];
 static llist_free_entry* freeListFirst = nullptr;
 
 static void llist_insert_before(llist_free_entry* target, void* address, size_t size);
 static void llist_insert_after(llist_free_entry* target, void* address, size_t size);
 static void llist_remove(llist_free_entry* entry);
 
+static size_t llist_shrink(llist_free_entry* entry, size_t amount);
+static void llist_grow_up(llist_free_entry* entry, size_t amount);
+
 void heap_free_llist_init(void* location, size_t heapSize)
 {
-    llist_free_entry allHeap{};
-    allHeap.address = location;
-    allHeap.size = heapSize;
-    allHeap.prev = allHeap.next = nullptr;
+    kernel_assert(heapSize >= sizeof(llist_free_entry));
 
-    freeList[0] = allHeap;
-    freeListFirst = freeList;
+    freeListFirst = (llist_free_entry*)location;
+    freeListFirst->size = heapSize;
+    freeListFirst->prev = freeListFirst->next = nullptr;
 }
 
 void* heap_free_llist_alloc(size_t size)
 {
-        if (size == 0)
+    if (size == 0)
         return nullptr;
     
     size += sizeof(alloc_header);
+    size = max(size, sizeof(llist_free_entry));
 
     llist_free_entry* suitable = freeListFirst;
     while (suitable->size < size)
@@ -69,11 +54,8 @@ void* heap_free_llist_alloc(size_t size)
 
     kernel_assert(suitable != nullptr, "Can't allocate more heap, no suitable free block of size %d.", size - sizeof(alloc_header));
 
-    void* addr = suitable->address;
-    suitable->shrink(size);
-
-    if (suitable->size == 0)
-        llist_remove(suitable);
+    void* addr = suitable;
+    size = llist_shrink(suitable, size);
 
     alloc_header* header = (alloc_header*)addr;
     header->checksum = ALLOC_HEADER_CHECKSUM;
@@ -87,14 +69,23 @@ void heap_free_llist_free(void* addr)
     addr = addr - sizeof(alloc_header);
     alloc_header* header = (alloc_header*)addr;
 
-    kernel_assert(header->checksum != ALLOC_HEADER_FREED_CHECKSUM, "Freeing an already freed memory.");
     kernel_assert(header->checksum == ALLOC_HEADER_CHECKSUM && header->size > sizeof(alloc_header));
+    header->checksum = 0;
 
     llist_free_entry* entry = freeListFirst;
-    do
+    if (entry == nullptr)
     {
-        int topDistance = (size_t)entry->address - ((size_t)addr + header->size);
-        int bottomDistance = (size_t)addr - ((size_t)entry->address + entry->size);
+        llist_free_entry* newEntry = (llist_free_entry*)addr;
+        newEntry->size = header->size;
+
+        freeListFirst = newEntry;
+        return;
+    }
+
+    while (entry != nullptr)
+    {
+        int topDistance = (size_t)entry - ((size_t)addr + header->size);
+        int bottomDistance = (size_t)addr - ((size_t)entry + entry->size);
 
         kernel_assert(!(topDistance < 0 && topDistance >= -header->size), "Freeing area overlaps with already free data. May be a double-free.");
         kernel_assert(!(bottomDistance < 0 && bottomDistance >= -header->size), "Freeing area overlaps with already free data. May be a double-free.");
@@ -102,7 +93,7 @@ void heap_free_llist_free(void* addr)
         if (bottomDistance == 0)
         {
             entry->size += header->size;
-            if (entry->next != nullptr && ((size_t)entry->next->address - ((size_t)entry->address + entry->size)) == 0)
+            if (entry->next != nullptr && ((size_t)entry->next - ((size_t)entry + entry->size)) == 0)
             {
                 entry->size += entry->next->size;
                 llist_remove(entry->next);
@@ -114,7 +105,7 @@ void heap_free_llist_free(void* addr)
         if (topDistance >= 0)
         {
             if (topDistance == 0)
-                entry->growUp(header->size);
+                llist_grow_up(entry, header->size);
             else
                 llist_insert_before(entry, addr, header->size);
             
@@ -129,12 +120,8 @@ void heap_free_llist_free(void* addr)
 
         entry = entry->next;
     } 
-    while (entry != nullptr);
 
     kernel_assert(entry != nullptr, "heap_free_llist_free() impossible branch reached. Failed to find a suitable method to reclaim memory.");
-
-    // This is usefull for debugging and error checking, so we can surely know if it's a freed memory portion.
-    header->checksum = ALLOC_HEADER_FREED_CHECKSUM;
 }
 
 size_t heap_free_llist_get_unused()
@@ -157,7 +144,7 @@ void heap_free_llist_dump_free_blocks()
     llist_free_entry* entry = freeListFirst;
     do
     {
-        kconsole::printf("%p, sized: %d B\n", entry->address, entry->size);
+        kconsole::printf("%p, sized: %d B\n", entry, entry->size);
         entry = entry->next;
     } 
     while (entry != nullptr);
@@ -165,14 +152,10 @@ void heap_free_llist_dump_free_blocks()
 
 void llist_insert_before(llist_free_entry* target, void* address, size_t size)
 {
-    llist_free_entry* newEntry = nullptr;
-    for (size_t i = 0; i < FREE_LIST_MAXSIZE; i++)
-    {
-        if (freeList[i].is_empty())
-            newEntry = freeList + i;
-    }
+    kernel_assert(size >= sizeof(llist_free_entry));
 
-    kernel_assert(newEntry != nullptr, "No available entries in the free linked list.");
+    llist_free_entry* newEntry = (llist_free_entry*)address;
+    newEntry->size = size;
 
     if (freeListFirst == target)
         freeListFirst = newEntry;
@@ -182,21 +165,14 @@ void llist_insert_before(llist_free_entry* target, void* address, size_t size)
     newEntry->prev = target->prev;
     newEntry->next = target;
     target->prev = newEntry;
-
-    newEntry->address = address;
-    newEntry->size = size;
 }
 
 void llist_insert_after(llist_free_entry* target, void* address, size_t size)
 {
-    llist_free_entry* newEntry = nullptr;
-    for (size_t i = 0; i < FREE_LIST_MAXSIZE; i++)
-    {
-        if (freeList[i].is_empty())
-            newEntry = freeList + i;
-    }
+    kernel_assert(size >= sizeof(llist_free_entry));
 
-    kernel_assert(newEntry != nullptr, "No available entries in the free linked list.");
+    llist_free_entry* newEntry = (llist_free_entry*)address;
+    newEntry->size = size;
 
     if (target->next != nullptr)
     {
@@ -205,9 +181,6 @@ void llist_insert_after(llist_free_entry* target, void* address, size_t size)
     newEntry->prev = target;
     newEntry->next = target->next;
     target->next = newEntry;
-
-    newEntry->address = address;
-    newEntry->size = size;
 }
 
 void llist_remove(llist_free_entry* entry)
@@ -218,6 +191,43 @@ void llist_remove(llist_free_entry* entry)
     entry->prev->next = entry->next;
     entry->next->prev = entry->prev;
 
-    entry->address = nullptr;
     entry->size = 0;
+}
+
+size_t llist_shrink(llist_free_entry* block, size_t amount)
+{
+    kernel_assert(block->size >= amount);
+
+    size_t left = block->size - amount;
+    if (left < sizeof(llist_free_entry))
+    {
+        size_t blockSize = block->size;
+        llist_remove(block);
+
+        return blockSize;
+    }
+
+    llist_free_entry* newPlace = (llist_free_entry*)((uint8*)block + amount);
+    *newPlace = *block;
+    
+    newPlace->size = left;
+    newPlace->prev->next = newPlace;
+    newPlace->next->prev = newPlace;
+
+    if (block == freeListFirst)
+        freeListFirst = newPlace;
+    return amount;
+}
+
+void llist_grow_up(llist_free_entry* entry, size_t amount)
+{
+    llist_free_entry* newPlace = (llist_free_entry*)((uint8*)entry - amount);
+    *newPlace = *entry;
+    
+    newPlace->size += amount;
+    newPlace->prev->next = newPlace;
+    newPlace->next->prev = newPlace;
+
+    if (entry == freeListFirst)
+        freeListFirst = newPlace;
 }
